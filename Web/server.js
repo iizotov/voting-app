39b2c3fded4eb3130a28b77bebf268acce3973c5
Igor Izotov <igor.izotov@microsoft.com>
2017-02-07 18:33:56 +1100

    added devicekey caching on the browser side

--- Web/server.js
+++ Web/server.js
@@ -32,9 +32,38 @@ var completedCallback = function(err, res) {
 
 var res;
 
+app.post('/api/getDevice', function(req, res) {
+    console.log('getDevice command received from: ' + req.body.devicehash);
+	var device = new iothub.Device(null);
+	device.deviceId = req.body.devicehash;
+	registry.create(device, function(err, deviceInfo, res2) {
+		if (err) {
+			registry.get(device.deviceId, function(err, deviceInfo, res3) {
+				res.end(deviceInfo.authentication.symmetricKey.primaryKey);
+			});
+			console.log('getting identity');
+		}
+	})
+});
 
-app.post('/api/command', function(req, res) {
-    console.log('command received: ' + req.body.command + ',' + req.body.devicehash);
+app.post('/api/getMessages', function(req, res) {
+    console.log('getMessages command received from: ' + req.body.devicehash + ',' + req.body.devicekey);
+	var deviceKey = req.body.devicekey;
+	var device = new iothub.Device(null);
+	device.deviceId = req.body.devicehash;
+
+		
+	// Code goes here
+	
+	client.on('message', function (msg) {
+		console.log('Id: ' + msg.messageId + ' Body: ' + msg.data);
+		client.complete(msg, printResultFor('completed'));
+	});
+	
+});
+
+app.post('/api/vote', function(req, res) {
+    console.log('vote command received from: ' + req.body.command + ',' + req.body.devicehash + ',' + req.body.devicekey);
 
 	var vote = req.body.command;
 	var deviceKey = req.body.devicekey;
@@ -45,24 +74,22 @@ app.post('/api/command', function(req, res) {
 		registry.create(device, function(err, deviceInfo, res) {
 			if (err) {
 				registry.get(device.deviceId, sendMessage);
-				console.log('getting identity');
 			}
 			if (deviceInfo) {
 				sendMessage(err, deviceInfo, res, '', '');	
 			}
 		})
 	} else {
-		sendMessage(err, '', res, req.body.devicehash, req.body.devicekey); 	
+		sendMessage('', '', res, req.body.devicehash, req.body.devicekey); 	
 	}
 	 
 
 	function sendMessage(err, deviceInfo, res, deviceId, deviceAccessKey) {
 		var connectionString = '';
 		if (deviceId && deviceAccessKey) {
-			connectionString = 'HostName=' + hostName + ';DeviceId=' + deviceInfo.deviceId + ';SharedAccessKey=' + deviceInfo.authentication.symmetricKey.primaryKey;
+			connectionString = 'HostName=' + hostName + ';DeviceId=' + deviceId + ';SharedAccessKey=' + deviceAccessKey;
 			console.log('Device ID: ' + deviceId);
 			console.log('Device key - cached: ' + deviceAccessKey);
-			deviceKey = deviceAccessKey;
 		}
 		else if (deviceInfo) {
 			console.log('Device ID: ' + deviceInfo.deviceId);
@@ -70,18 +97,17 @@ app.post('/api/command', function(req, res) {
 			connectionString = 'HostName=' + hostName + ';DeviceId=' + deviceInfo.deviceId + ';SharedAccessKey=' + deviceInfo.authentication.symmetricKey.primaryKey;
 			deviceKey = deviceInfo.authentication.symmetricKey.primaryKey;
 		}
+		console.log(connectionString);
 		var client = clientFromConnectionString(connectionString);
 		var message = new Message("{'vote':" + vote + '}');
 		console.log("Sending message: " + message.getData());
-		client.sendEvent(message, printResultFor('send'));		
+		client.sendEvent(message, printResultFor('send'));
+
 	 }
 	//need to create device identity here
 	//console.log('Sending message: ' + data);
 	//iotHubClient.send(deviceId, message, printResultFor('send'));
     
-	
-	
-
     // Helper function to print results in the console
     function printResultFor(op) {
         return function printResult(err, res) {
