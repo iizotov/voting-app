564d25062c9a9c0416457e41fcae01fe8ad3f645
Igor Izotov <igor.izotov@microsoft.com>
2017-02-09 09:48:28 +1100

    webjob bug fix

--- Web/App_Data/jobs/triggered/listDevices/run.js
+++ Web/App_Data/jobs/triggered/listDevices/run.js
@@ -1,4 +1,6 @@
 var iotHubConnString;
+var nconf = require('nconf');
+var Client = require('azure-iothub').Client;
 
 if(process.env.IoTHubConnectionString) {
 	nconf.argv().env().file('./config.json');
@@ -9,3 +11,22 @@ if(process.env.IoTHubConnectionString) {
 }
 
 console.log(iotHubConnString);
+var serviceClient = Client.fromConnectionString(iotHubConnString);
+var registry = iothub.Registry.fromConnectionString(iotHubConnString);
+
+
+console.log('**listing devices...');
+registry.list(function (err, deviceList) {
+serviceClient.open(function (err) {	
+	if (err) {
+		console.error('Could not connect: ' + err.message);
+	} else {
+		deviceList.forEach(function (device) {
+			var key = device.authentication ? device.authentication.symmetricKey.primaryKey : '<no primary key>';
+			console.log(device.deviceId + ': ' + key);
+		});
+	}
+});
+});
+
+
